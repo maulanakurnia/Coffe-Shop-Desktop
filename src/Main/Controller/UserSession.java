4190abd7b7728aee33db570cb7a42ef78f0a8ac2
maulanaakurniaa@yahoo.com <maulanaakurniaa@yahoo.com>
2020-05-17 05:22:36 +0800

    Menambah Session

--- src/Main/Controller/UserSession.java
+++ src/Main/Controller/UserSession.java
@@ -1,28 +1,31 @@
 package Main.Controller;
 
 public class UserSession {
-		private static String id_user, nama;
-		private static int role;
+	private static String id_user, nama, idPemesanan;
+	private static int role;
 
-		public static String getId_user(){
-				return id_user;
-		}
-		public static void setId_user(String vid_user){
-				id_user = vid_user;
-		}
+	public static String getId_user(){
+			return id_user;
+	}
+	public static void setId_user(String vid_user){
+			id_user = vid_user;
+	}
 
-		public static String getNama(){
-				return nama;
-		}
+	public static String getNama(){
+			return nama;
+	}
 
-		public static void setNama(String vnama) {
-				nama = vnama;
-		}
+	public static void setNama(String vnama) {
+			nama = vnama;
+	}
 
-		public static int getRole(){
-				return role;
-		}
-		public static void setRole(int vrole){
-				role = vrole;
-		}
+	public static int getRole(){
+			return role;
+	}
+	public static void setRole(int vrole){
+			role = vrole;
+	}
+
+	public static String getIdPemesanan(){return idPemesanan;}
+	public static void setIdPemesanan(String vidPemesanan){ idPemesanan = vidPemesanan;}
 }
