62bc227c47253b07c54e7087297a3cb89fa8a2df
maulanaakurniaa@yahoo.com <maulanaakurniaa@yahoo.com>
2020-05-17 13:22:17 +0800

    Mengubah deklarasi total serta method total menjadi static

--- src/Main/Controller/DataProduk.java
+++ src/Main/Controller/DataProduk.java
@@ -1,24 +1,15 @@
 package Main.Controller;
 
 public class DataProduk {
-	String idKopi;
-	int stok;
-	float harga;
-	String namaKopi;
+	private String idKopi, namaKopi;
+	private int stok, harga;
+	private static int total;
 
-    public DataProduk() {
-
-    }
-
-    public String getIdKopi(){
-				return idKopi;
-		}
+    public String getIdKopi(){ return idKopi;}
 	public String getNamaKopi(){
 			return namaKopi;
 	}
-	public float getHarga(){
-			return harga;
-	}
+	public int getHarga(){ return harga; }
 	public int getStok(){
 			return stok;
 	}
@@ -27,10 +18,13 @@ public class DataProduk {
 	public void setNamaKopi(String vnamaKopi){
 			namaKopi = vnamaKopi;
 	}
-	public void setHarga(float vHarga){
+	public void setHarga(int vHarga){
 			harga = vHarga;
 	}
-	public void setStok(int vstok){
-			stok = vstok;
-	}
+	public void setStok(int vstok){ stok = vstok; }
+
+	public static int getTotal(){ return total;}
+	public static void setTotal(int vtotal){total += vtotal;}
+
+
 }
