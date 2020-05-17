94827b1bf52341c52243e5fa2bd5c40105431e50
maulanaakurniaa@yahoo.com <maulanaakurniaa@yahoo.com>
2020-05-17 13:21:40 +0800

    tambah method bayar

--- src/Main/GUI/Bayar.java
+++ src/Main/GUI/Bayar.java
@@ -1,12 +1,30 @@
 package Main.GUI;
 
 import Main.Controller.DataProduk;
+import Main.Controller.Koneksi;
 import Main.Controller.UserSession;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class Bayar {
+    Koneksi koneksi = new Koneksi();
+    private static final SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+    ResultSet resultSet;
+    Statement statement;
+    NumberFormat nf = NumberFormat.getInstance(new Locale("da", "DK"));
+
     JFrame window         = new JFrame("PEMBAYARAN");
     DataProduk produk     = new DataProduk();
     JLabel lIdPesanan     = new JLabel("ID Pesanan");
@@ -14,18 +32,40 @@ public class Bayar {
     JLabel lTotal         = new JLabel("Total");
     JTextField fTotal     = new JTextField();
     JLabel lKursi         = new JLabel("KURSI");
-    JComboBox cKursi      = new JComboBox();
+    JComboBox<String> cKursi = new JComboBox<>();
+
+    JButton bBayar  = new JButton("Bayar");
+    JButton bKembali = new JButton("Kembali");
 
 
     public Bayar(){
-        new TabelPesanan();
+        TabelPesanan tabelPesanan = new TabelPesanan();
         initComponents();
 
         window.setLayout(null);
-        window.setSize(420, 200);
+        window.setSize(420, 210);
         window.setVisible(true);
         window.setLocationRelativeTo(null);
         window.setResizable(false);
+
+        bBayar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bayar();
+                tabelPesanan.window.setVisible(false);
+                window.setVisible(false);
+                new Menu();
+            }
+         });
+
+        bKembali.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tabelPesanan.window.setVisible(false);
+                window.setVisible(false);
+                new Menu();
+            }
+        });
     }
 
     private void initComponents() {
@@ -44,7 +84,7 @@ public class Bayar {
         lTotal.setForeground(new Color(255, 255, 255));
             window.add(fTotal);
             fTotal.setBounds(140,60,210,25);
-            fTotal.setText(String.valueOf(produk.getTotal()));
+            fTotal.setText(" Rp."+nf.format(DataProduk.getTotal()));
             fTotal.setEditable(false);
 
         window.add(lKursi);
@@ -52,6 +92,34 @@ public class Bayar {
         lKursi.setForeground(new Color(255, 255, 255));
             window.add(cKursi);
             cKursi.setBounds(140,95,210,25);
+            cKursi.addItem(" --- PILIH NOMOR KURSI ---");
+            for(int i=1; i<25; i++) {
+                cKursi.addItem("00"+i);
+            }
+        window.add(bBayar);
+        bBayar.setBounds(140,130,110,25);
+        bBayar.setForeground(new Color(255,255,255));
+        bBayar.setBackground(new Color(58, 133, 86));
 
+        window.add(bKembali);
+        bKembali.setBounds(260,130,90,25);
+        bKembali.setForeground(new Color(255,255,255));
+        bKembali.setBackground(new Color(145, 141, 58));
+    }
+
+    private void bayar(){
+        try{
+            statement = koneksi.getConnection().createStatement();
+            String sql = "INSERT INTO detail_pesanan VALUES(default,'" + UserSession.getIdPemesanan() + "','" + cKursi.getSelectedItem() + "','" + time.format(timestamp) +"','" + DataProduk.getTotal() + "','TELAH DIBAYAR')";
+            int disimpan = statement.executeUpdate(sql);
+            if(disimpan == 1){
+                JOptionPane.showMessageDialog(null, "Terimakasih telah membayar pesanan!","Peringatan",JOptionPane.WARNING_MESSAGE);
+            }
+            statement.close();
+        }catch (SQLException sqlError) {
+            JOptionPane.showMessageDialog(null, "Data Gagal Ditampilkan" + sqlError);
+        } catch (ClassNotFoundException classError) {
+            JOptionPane.showMessageDialog(null, "Driver tidak ditemukan !!");
+        }
     }
 }
