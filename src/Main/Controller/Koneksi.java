2346ea351e704e70709522231e56edd7465dfc44
Maulana Kurnia <50382874+maulanakurnia@users.noreply.github.com>
2020-05-18 19:51:26 +0800

    Menambahkan timezone sql

--- src/Main/Controller/Koneksi.java
+++ src/Main/Controller/Koneksi.java
@@ -8,7 +8,7 @@ public class Koneksi {
 		public Connection getConnection() throws ClassNotFoundException, SQLException {
 				Class.forName("com.mysql.cj.jdbc.Driver");
 
-				String url = "jdbc:mysql://localhost/db_WarungKopi";
+				String url = "jdbc:mysql://localhost/db_WarungKopi?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 				Connection koneksi = DriverManager.getConnection(url, "root", "root");
 				return koneksi;
 		}
